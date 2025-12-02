7b3fef00dbcb5107f256684063c63d2b61eca386
Manishachoudhary99 <mc6826263@gmail.com>
2025-12-02 14:23:45 +0530

    moved saampleShipments as centralised data in separate file and imported in dashboard, also corrected a small bug moved pagination component out of table

---
+++ src/sampleShipments.js
@@ -0,0 +1,14 @@
+export const sampleShipments =[
+  {id:'SHP001',product:'Iphone14',source:'Mumbai',destination:'Delhi',status:'In Transit',lastUpdated:'2025-11-28'},
+  {id:'SHP002',product:'Iphone15',source:'Bangalore',destination:'Gujrat',status:'Delivered',lastUpdated:'2025-11-27'},
+  {id:'SHP003',product:'Iphone15',source:'Rajasthan',destination:'Punjab',status:'Delayed',lastUpdated:'2025-11-29'},
+  {id:'SHP004',product:'Iphone17',source:'Assam',destination:'Kerala',status:'In Transit',lastUpdated:'2025-11-30'},
+  {id:'SHP005',product:'Iphone18',source:'Mumbai',destination:'Delhi',status:'In Transit',lastUpdated:'2025-12-01'},
+  {id:'SHP006',product:'Iphone15',source:'Bangalore',destination:'Gujrat',status:'Delivered',lastUpdated:'2025-12-02'},
+  {id:'SHP007',product:'phone17',source:'Rajasthan',destination:'Punjab',status:'Delayed',lastUpdated:'2025-11-28'},
+  {id:'SHP008',product:'Vivo',source:'Assam',destination:'Kerala',status:'In Transit',lastUpdated:'2025-11-30'},
+  {id:'SHP009',product:'Samsung',source:'Mumbai',destination:'Delhi',status:'In Transit',lastUpdated:'2025-10-28'},
+  {id:'SHP010',product:'Readmi',source:'Bangalore',destination:'Gujrat',status:'Delivered',lastUpdated:'2025-10-27'},
+  {id:'SHP011',product:'Nothing',source:'Rajasthan',destination:'Punjab',status:'Delayed',lastUpdated:'2025-09-29'},
+  {id:'SHP012',product:'Nokia',source:'Assam',destination:'Kerala',status:'In Transit',lastUpdated:'2025-09-30'},
+];
