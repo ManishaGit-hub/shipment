3a16890883c30c6abbd59bc682d9a391cdf662bf
Manishachoudhary99 <mc6826263@gmail.com>
2025-12-02 17:09:47 +0530

    we added other details to show on details page like timeline,logs,estimated delivery and image

--- src/ShipmentDetailsPage.jsx
+++ src/ShipmentDetailsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams,useNavigate } from 'react-router-dom'
 import { shipmentSampleItems } from './shipmentSampleItems'
+import Style from './ShipmentDetailsPage.module.css'
 
 const ShipmentDetailsPage = () => {
     const {id} = useParams();
@@ -21,7 +22,37 @@ const ShipmentDetailsPage = () => {
                 <p><strong>Destination:</strong>{item.destination}</p>
                 <p><strong>Status:</strong><span className={`badge ms-2 ${item.status === 'Delivered' ? 'bg-success':item.status === 'In Transit' ? 'bg-info text-dark':'bg-danger'}`}>{item.status}</span></p>
             </div>
+            <hr className='my-4'/>
+
+            {/*image*/}
+            <div className="text-center mb-4">
+                <img src={item.image} alt={item.product} className="img-fluid rounded shadow" style={{maxWidth:"260px"}}/>
+            </div>
+            {/*Timeline*/}
+            <h4 className="mb-3">Shipping Timeline</h4>
+            <div className="d-flex justify-content-between mb-4">
+                {["Ordered","Packed","Dispatched","In Transit","Delivered"].map((step,index)=>(
+                    <div key={index}>
+                        <div className={`${Style.timelineDot} ${item.status === step ? "text-primary":index<["Ordered","Packed","Dispatched","In Transit","Delivered"].indexOf(item.status)?"text-success":"text-secondary"}`}>
+                            ●
+                        </div>
+                       <small className="fw-semibold">{step}</small>
+                    </div>
+                ))}
+            </div>
+            {/*Log Events*/}
+            <h4 className="mt-3">Recent Updates</h4>
+            <ul className="list-group mb-4">
+                {item.logs.slice(-5).map((log,i)=>(
+                    <li key={i} className="list-group-item">{log.date}----: <strong>{log.message}</strong></li>
+                ))}
+            </ul>
+            {/*Estimated Delivery*/}
+            <div className="alert alert-info text-center fw-bold">
+                Estimated Delivery : {item.estimatedDelivery}
+            </div>
         </div>
+
     </>
   )
 }
